2be93933ccbcae31b7aa07c8c97984d654dcff0a
Aasmaan Gupta <aasmaanplayer@gmail.com>
2025-04-13 22:33:54 +0530

    something

--- diayn/models.py
+++ diayn/models.py
@@ -15,26 +15,18 @@ class Discriminator(nn.Module, ABC):
     def __init__(self, n_states, n_skills, n_hidden_filters=300):
         super(Discriminator, self).__init__()
         self.input_dim = n_states
-        self.hidden1 = nn.Linear(n_states, 300)
+        self.hidden1 = nn.Linear(n_states, n_hidden_filters)
         init_weight(self.hidden1)
 
-        self.hidden2 = nn.Linear(300, 200)
+        self.hidden2 = nn.Linear(n_hidden_filters, n_hidden_filters)
         init_weight(self.hidden2)
 
-        self.hidden3 = nn.Linear(200, 100)
-        init_weight(self.hidden3)
-
-        self.hidden4 = nn.Linear(100, 64)
-        init_weight(self.hidden4)
-
-        self.q = nn.Linear(64, n_skills)
+        self.q = nn.Linear(n_hidden_filters, n_skills)
         init_weight(self.q, initializer="xavier uniform")
 
     def forward(self, states):
         x = F.relu(self.hidden1(states))
         x = F.relu(self.hidden2(x))
-        x = F.relu(self.hidden3(x))
-        x = F.relu(self.hidden4(x))
         logits = self.q(x)
         return logits
 
@@ -44,24 +36,16 @@ class QNetwork(nn.Module):
         input_dim = np.prod(env.observation_space.shape) + n_skills
         n_actions = env.action_space.n
 
-        self.fc1 = nn.Linear(input_dim, 300)
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
         init_weight(self.fc1)
 
-        self.fc2 = nn.Linear(300, 200)
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
         init_weight(self.fc2)
 
-        self.fc3 = nn.Linear(200, 100)
-        init_weight(self.fc3)
-
-        self.fc4 = nn.Linear(100, 64)
-        init_weight(self.fc4)
-
-        self.out = nn.Linear(64, n_actions)
+        self.out = nn.Linear(hidden_dim, n_actions)
         init_weight(self.out, "xavier uniform")
 
     def forward(self, x):
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
-        x = F.relu(self.fc3(x))
-        x = F.relu(self.fc4(x))
         return self.out(x)
